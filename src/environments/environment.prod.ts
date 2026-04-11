d605ec30dd47e650a3fdfb8983e0a54736774457
Cris <krontroth@gmail.com>
2026-04-11 05:09:45 -0500

    gjhg

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-command-production.up.railway.app'
+  apiUrl: 'https://backend-command-production.up.railway.app/api'
 };
